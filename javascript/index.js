f21ec96165cdac52303540829f73437480a8f3ee
Antonia Verdi <averdi@gm.slc.edu>
2016-04-24 23:00:12 -0500

    javascript added

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+function getValue(){
+    var myArray= ["BRILLIANT","BORDERLINE GENIUS","SUPER ENTHUSIASTIC", "TALENTED", "MAGNIFICENT", "SMART", "FUNNY", "COOL"];
+    var blanks = document.getElementsByClassName("message");
+
+    for(i=0; i<blanks.length; i++){
+      var random = Math.floor(Math.random() * myArray.length);
+      blanks[i].innerHTML = myArray[random];
+      myArray.splice(random, 1);
+    }
+}
